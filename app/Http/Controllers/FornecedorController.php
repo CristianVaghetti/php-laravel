a9b08d5681cdd7ab4ee867bc6fde73ce612d1643
CristianVaghetti <vaghetticristian@gmail.com>
2021-11-01 22:26:34 -0300

    adicionado novos testes dentro do blade, array dentro de arry

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -9,9 +9,28 @@ class FornecedorController extends Controller
     public function index () {
         //$fornecedores = ['fornecedor 1','2','3','4','5','6'];
         $fornecedores = [
-            0 => ['nome' => 'Fornecedor1', 'status' => 'N'],
-            1 => ['nome' => 'Fornecedor2', 'status' => 'S']
-        ];
+            0 => [
+                'nome' => 'Fornecedor1', 
+                'status' => 'N',
+                'cnpj' => '00.00.00/001',
+                'ddd' => '11',
+                'telefone' => '000000000'
+            ],
+            1 => [
+                'nome' => 'Fornecedor2',
+                'status' => 'S',
+                'cnpj' => '10.10.10/001',
+                'ddd' => '21',
+                'telefone' => '000000001'
+            ],
+            2 => [
+                'nome' => 'Fornecedor3',
+                'status' => 'N',
+                'cnpj' => '20.20.20/002',
+                'ddd' => '13',
+                'telefone' => '000000002'
+            ]
+            ];
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
