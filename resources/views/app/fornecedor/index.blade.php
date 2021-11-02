62ceee39a191dfd725e380759c51a1104526bc9f
CristianVaghetti <vaghetticristian@gmail.com>
2021-11-01 21:17:29 -0300

    add de nova rota e controller e encaminhamento de parametros

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,13 @@
+<h3>FORNECEDOR</H3>
+
+{{'Texto de teste'}}
+<?= 'Texto de teste' ?>
+{{-- Fica o comentário que o interpretador do blade descarta --}}
+
+@php
+    // comentario 1 linha
+    /*
+        comentarios multiplas linhas
+    */
+    echo 'Texto de teste';
+@endphp
