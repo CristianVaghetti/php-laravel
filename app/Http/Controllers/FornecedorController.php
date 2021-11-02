5ae097dc8aacdf590703cf9842e9c92a0117a73c
CristianVaghetti <vaghetticristian@gmail.com>
2021-11-01 21:58:49 -0300

    uso de comandos php dentro do blade

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index () {
-        return view('app.fornecedor.index');
+        //$fornecedores = ['fornecedor 1','2','3','4','5','6'];
+        $fornecedores = [
+            0 => ['nome' => 'Fornecedor1', 'status' => 'N'],
+            1 => ['nome' => 'Fornecedor2', 'status' => 'S']
+        ];
+        return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -11,3 +11,21 @@
     */
     echo 'Texto de teste';
 @endphp
+
+{{--@dd($fornecedores) // imprime array no blade--}} 
+
+{{--@if(count($fornecedores) > 0 && count($fornecedores) < 5) {
+    <h3>Existe alguns fornecedores</h3>
+} @elseif(count($fornecedores) > 5) {
+    <h3>Existe vários fornecedores</h3>
+} @else {
+    <h3>Ainda não existe fornecedores</h3>
+}
+@endif--}}
+<br>
+Fornecedor: {{$fornecedores[0]['nome']}}<br>
+Status: {{$fornecedores[0]['status']}}<br>
+
+@if($fornecedores[0]['status'] == 'N')
+    Fornecedor inativo
+@endif
